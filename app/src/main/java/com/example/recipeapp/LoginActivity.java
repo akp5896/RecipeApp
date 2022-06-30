6d3b1631c6381cbdd84f2eb9196c4cef2b96a6fe
Anastasiia Popova <stacypopova123@fb.com>
2022-06-30 09:52:17 -0700

    moved context-reliable functions from ViewModel to View

--- app/src/main/java/com/example/recipeapp/LoginActivity.java
+++ app/src/main/java/com/example/recipeapp/LoginActivity.java
@@ -2,7 +2,7 @@ package com.example.recipeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
-import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.content.Context;
 import android.content.Intent;
@@ -11,27 +11,27 @@ import android.util.Log;
 import android.widget.Toast;
 
 import com.example.recipeapp.viewmodels.LoginViewModel;
-import com.parse.LogInCallback;
-import com.parse.ParseException;
 import com.parse.ParseUser;
 import com.example.recipeapp.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
     private static final String TAG = "LOGIN ACTIVITY";
     ActivityLoginBinding binding;
+    LoginViewModel vm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_login);
-        binding.setViewModel(new LoginViewModel());
+        vm = ViewModelProviders.of(this).get(LoginViewModel.class);
+        binding.setViewModel(vm);
 
         if(ParseUser.getCurrentUser() != null) {
             startActivity(new Intent(LoginActivity.this, MainActivity.class));
         }
 
-        binding.getViewModel().signInSuccessful.observe(this, e -> {
+        vm.isLogInSuccessful.observe(this, e -> {
             Context context = getApplicationContext();
             if(e != null) {
                 Log.i(TAG, "Issue with login" + e);
@@ -40,5 +40,7 @@ public class LoginActivity extends AppCompatActivity {
             }
             startActivity(new Intent(context, MainActivity.class));
         });
+
+        vm.startSignUp.observe(this, startSignUp -> startActivity(new Intent(LoginActivity.this, SignUpActivity.class)));
     }
 }
--- app/src/main/java/com/example/recipeapp/viewmodels/LoginViewModel.java
+++ app/src/main/java/com/example/recipeapp/viewmodels/LoginViewModel.java
@@ -1,47 +1,33 @@
 package com.example.recipeapp.viewmodels;
 
-import android.content.Context;
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Toast;
-
-import androidx.databinding.BaseObservable;
 import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
 
-import com.example.recipeapp.MainActivity;
-import com.example.recipeapp.SignUpActivity;
+import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
 
-import kotlin.reflect.KClass;
-
-public class LoginViewModel extends BaseObservable {
-
-    private static final String TAG = "Logging in: ";
+public class LoginViewModel extends ViewModel {
 
     private String username;
     private String password;
 
-    public MutableLiveData<ParseException> signInSuccessful;
+
+    public MutableLiveData<Boolean> startSignUp = new MutableLiveData<>();
+    public MutableLiveData<ParseException> isLogInSuccessful = new MutableLiveData<>();
 
     public LoginViewModel() {
-
     }
 
     public void login() {
         ParseUser.logInInBackground(
                 username,
                 password,
-                (user, e) -> {
-                    signInSuccessful.setValue(e);
-                });
+                (user, e) -> isLogInSuccessful.setValue(e));
     }
 
-    public void signUp(View view) {
-        Context context = view.getContext();
-        context.startActivity(new Intent(context, SignUpActivity.class));
+    public void signUp() {
+        startSignUp.setValue(true);
     }
 
     public String getUsername() {
