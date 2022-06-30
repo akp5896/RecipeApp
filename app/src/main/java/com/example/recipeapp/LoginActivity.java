baa5bd9542ba0d4733eb74605ca55c704fdb6b7b
Anastasiia Popova <stacypopova123@fb.com>
2022-06-30 11:19:05 -0700

    exception handling moved to enum

--- app/src/main/java/com/example/recipeapp/LoginActivity.java
+++ app/src/main/java/com/example/recipeapp/LoginActivity.java
@@ -7,7 +7,6 @@ import androidx.lifecycle.ViewModelProviders;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Toast;
 
 import com.example.recipeapp.viewmodels.LoginViewModel;
@@ -17,30 +16,32 @@ import com.example.recipeapp.databinding.ActivityLoginBinding;
 public class LoginActivity extends AppCompatActivity {
     private static final String TAG = "LOGIN ACTIVITY";
     ActivityLoginBinding binding;
-    LoginViewModel vm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_login);
-        vm = ViewModelProviders.of(this).get(LoginViewModel.class);
-        binding.setViewModel(vm);
+        binding.setViewModel(ViewModelProviders.of(this).get(LoginViewModel.class));
 
         if(ParseUser.getCurrentUser() != null) {
             startActivity(new Intent(LoginActivity.this, MainActivity.class));
         }
 
-        vm.isLogInSuccessful.observe(this, e -> {
+        binding.getViewModel().isLogInSuccessful.observe(this, errorCode -> {
             Context context = getApplicationContext();
-            if(e != null) {
-                Log.i(TAG, "Issue with login" + e);
-                Toast.makeText(context, "Unable to login", Toast.LENGTH_SHORT).show();
-                return;
+            switch (errorCode) {
+                case SUCCESS:
+                    startActivity(new Intent(context, MainActivity.class));
+                    finish();
+                    break;
+                case PARSE_ERROR:
+                case PARSE_INVALID_CREDENTIALS:
+                default:
+                    Toast.makeText(context, "Unable to login: " + errorCode.message, Toast.LENGTH_SHORT).show();
             }
-            startActivity(new Intent(context, MainActivity.class));
         });
 
-        vm.startSignUp.observe(this, startSignUp -> startActivity(new Intent(LoginActivity.this, SignUpActivity.class)));
+        binding.getViewModel().startSignUp.observe(this, startSignUp -> startActivity(new Intent(LoginActivity.this, SignUpActivity.class)));
     }
 }
--- app/src/main/java/com/example/recipeapp/viewmodels/LoginViewModel.java
+++ app/src/main/java/com/example/recipeapp/viewmodels/LoginViewModel.java
@@ -1,5 +1,7 @@
 package com.example.recipeapp.viewmodels;
 
+import android.util.Log;
+
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
@@ -9,12 +11,14 @@ import com.parse.ParseUser;
 
 public class LoginViewModel extends ViewModel {
 
-    private String username;
-    private String password;
+    private static final String TAG = "Logging in: ";
+    private static final int INVALID_LOGIN_PARAMS = 101;
+    private String username = "";
+    private String password = "";
 
 
     public MutableLiveData<Boolean> startSignUp = new MutableLiveData<>();
-    public MutableLiveData<ParseException> isLogInSuccessful = new MutableLiveData<>();
+    public MutableLiveData<LoginResult> isLogInSuccessful = new MutableLiveData<>();
 
     public LoginViewModel() {
     }
@@ -23,7 +27,24 @@ public class LoginViewModel extends ViewModel {
         ParseUser.logInInBackground(
                 username,
                 password,
-                (user, e) -> isLogInSuccessful.setValue(e));
+                new LogInCallback() {
+                    @Override
+                    public void done(ParseUser user, ParseException e) {
+                        if(e != null) {
+                            Log.i(TAG, "Issue with login" + e);
+                            if(e.getCode() == INVALID_LOGIN_PARAMS
+                                    || e.getCode() == ParseException.USERNAME_MISSING
+                                    || e.getCode() == ParseException.PASSWORD_MISSING) {
+                                isLogInSuccessful.setValue(LoginResult.PARSE_INVALID_CREDENTIALS);
+                            }
+                            else {
+                                isLogInSuccessful.setValue(LoginResult.PARSE_ERROR);
+                            }
+                            return;
+                        }
+                        isLogInSuccessful.setValue(LoginResult.SUCCESS);
+                    }
+                });
     }
 
     public void signUp() {
@@ -44,5 +65,17 @@ public class LoginViewModel extends ViewModel {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public enum LoginResult {
+        SUCCESS("Logged in successfully"),
+        PARSE_ERROR("Unknown error"),
+        PARSE_INVALID_CREDENTIALS("Invalid username/password");
+
+        public final String message;
+
+        LoginResult(String message) {
+            this.message = message;
+        }
     }
 }
