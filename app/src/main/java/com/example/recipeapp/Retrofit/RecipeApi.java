2a11d5a985ff0c0174fd34826aea7da0b3b44af1
Anastasiia Popova <stacypopova123@fb.com>
2022-07-11 11:52:36 -0700

    API call added

--- app/src/main/java/com/example/recipeapp/Retrofit/RecipeApi.java
+++ app/src/main/java/com/example/recipeapp/Retrofit/RecipeApi.java
@@ -30,6 +30,8 @@ public interface RecipeApi {
     String RECIPE_INFORMATION_VALUE = "true";
     String SORT = "sort";
     String DIET = "diet";
+    String RANKING = "ranking";
+    String INGREDIENTS = "ingredients";
 
     @GET("/recipes/complexSearch")
     Call<Envelope<List<Recipe>>> getAllRecipes(@Query(API_KEY) String apiKey);
@@ -76,5 +78,10 @@ public interface RecipeApi {
                                                   @Query(NUMBER) int numberOfResults,
                                                   @Query(ADD_RECIPE_INFORMATION) String addRecipeInformation
     );
+
+    @GET("/recipes/findByIngredients")
+    Call<List<Recipe>> getRecipeByIngredients(@Query(API_KEY) String apiKey,
+                                              @Query(RANKING) int ranking,
+                                              @Query(INGREDIENTS) String ingredients);
 
 }
